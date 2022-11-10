9b1f860d45babcaad7653b7ee7a2b4e0171e54e1
kylemdavies <dav19010@byui.edu>
2022-11-10 13:31:57 -0700

    commit

--- Sandbox/fetch.js
+++ Sandbox/fetch.js
@@ -1,3 +1,33 @@
-const baseURL = "https://pokeapi.co/api/v2/pokemon/ditto"
-const results = fetch(baseURL);
-console.log(results);
+const baseURL = "https://pokeapi.co/api/v2/pokemon"
+const Pokemon = {
+    ditto : "/ditto"
+}
+
+let output = document.querySelector("#output")
+
+async function getPokemon(url, callback){
+    const response = await fetch(url);
+    if(response.ok) {
+    const data = await response.json();
+    // console.log(data);
+    callback(data)
+    }
+}
+
+function pokemonTemplate(pokemon) {
+    const html = `<h2>${pokemon.name.toUpperCase()} : #${pokemon.id}</h2>
+                <img src="${pokemon.sprites.other.home.front_default}" alt="Image of ${pokemon.name}">`
+    return html
+}   
+
+function doStuff(data) {
+    console.log(data)
+    output.innerHTML = pokemonTemplate(data);
+}
+
+function doStuffList(data){
+    
+}
+
+getPokemon(baseURL + Pokemon.ditto, doStuff);
+getPokemon(baseURL + "?limit=100", doStuffList)
